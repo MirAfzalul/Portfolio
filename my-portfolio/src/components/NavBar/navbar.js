02093f1d3cd9e6e8f52284243bcd1e452d02b446
MirAfzalul <mirafzalulbashar@gmail.com>
2023-10-23 23:55:26 -0400

    nav bar added

---
+++ my-portfolio/src/components/NavBar/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './navbar.css'
+import logo from'../../assets/mir-logo.png'
+import message from '../../assets/contact-form.png'
+import {Link} from 'react-scroll'
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <img src={logo} alt='Logo' className='logo'/>
+        <div className='desktopMenu'>
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>About</Link>
+            <Link className='desktopMenuListItem'>Portfolio</Link>
+            <Link className='desktopMenuListItem'>Clients</Link>
+        </div>
+        <button className='desktopMenuBtn'>
+            <img src={message}alt='' className='desktopMenuImg'/>
+            Contact Me
+        </button>
+    </nav>
+  )
+}
+
+export default Navbar
