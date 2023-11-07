fafda5e41b3955843b6baf4dde34375ccd286809
MirAfzalul <mirafzalulbashar@gmail.com>
2023-11-07 15:50:50 -0500

    made few changes on skill page

--- my-portfolio/src/components/NavBar/navbar.js
+++ my-portfolio/src/components/NavBar/navbar.js
@@ -4,7 +4,7 @@ import logo from'../../assets/mir-logo.png'
 import night from '../../assets/night.png'
 import {Link} from 'react-scroll'
 const Navbar = () => {
-  const[showMenu, setShowmenu]=useState(false );
+  const[showMenu, setShowmenu]=useState(false);
   return (
     <nav className='navbar'>
         <img src={logo} alt='Logo' className='logo'/>
@@ -12,7 +12,7 @@ const Navbar = () => {
             <Link activeClass='active' to ='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
             <Link  activeClass='active' to ='skills' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>About</Link>
             <Link  activeClass='active' to ='works' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
-            <Link  className='desktopMenuListItem'>Skills</Link>
+            <Link activeClass='active' to ='expertise' spy={true} smooth={true} offset={-100} duration={500} onClick={()=>setShowmenu(false)} className='desktopMenuListItem'>Skills</Link>
         </div>
         <button className='desktopMenuBtn'>
           
@@ -24,7 +24,7 @@ const Navbar = () => {
             <Link activeClass='active' to ='intro' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowmenu(false)}>Home</Link>
             <Link  activeClass='active' to ='skills' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowmenu(false)}> About</Link>
             <Link  activeClass='active' to ='works' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowmenu(false)}>Portfolio</Link>
-            <Link className='ListItem' >Skills</Link>
+            <Link activeClass='active' to ='expertise' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowmenu(false)}>Skills</Link>
         </div>
     </nav>
   )
