439234a5448015deaae535a93c4c1c3b6b9e057a
MirAfzalul <mirafzalulbashar@gmail.com>
2023-11-16 01:51:15 -0500

    button animation added

--- my-portfolio/src/components/Contact/contact.js
+++ my-portfolio/src/components/Contact/contact.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import '../Contact/contact.css'
 import LinkedIn from '../../assets/linkedin.png'
 
 
 const Contact = () => {
-  
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_84dwawr', 'template_3w2eceq', form.current, 'ruPPCjvwvggT68QA3wAkN')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div id='contactPage'>
         <h1 id='contactMePageTitle'>Contact Me</h1>
         <span className='contactDesc'>Please Fill Out The Form</span>
-        <form className='contactForm'>
-            <input type='text' className='name' placeholder='Your Name'/>
-            <input type='text' className='email' placeholder='Your Email'/>
+
+        <form className='contactForm' ref={form} onSubmit={sendEmail}>
+            <input type='text' className='name' placeholder='Your Name'name='user_name'/>
+            <input type='text' className='email' placeholder='Your Email' name='your_email'/>
             <textarea className='msg' name='message' rows='5' placeholder='Your Message'></textarea>
             <button type='submit' value='Send' className='submitBtn'>Submit</button>
             <div className='Links'>
