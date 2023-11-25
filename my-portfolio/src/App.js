8384743b9eaa66758f45f45c5f6f356f2b1c1838
MirAfzalul <mirafzalulbashar@gmail.com>
2023-11-24 21:22:38 -0500

    intro page updated

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -7,7 +7,9 @@ import Navbar from "./components/NavBar/navbar";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 
+
 function App() {
+
   return (
     <div className="App">
       {/* <Header/> */}
--- my-portfolio/src/components/Intro/intro.js
+++ my-portfolio/src/components/Intro/intro.js
@@ -10,9 +10,9 @@ const Intro = () => {
     <section id='intro'>
         
         <div className='introContent' >
-        <img src={bg} alt='Profile' className='bg'/> 
+        {/* <img src={bg} alt='Profile' className='bg'/>  */}
             <span className='hello'>Hello,</span>
-            <span className='introText'>I'm <span className='introName'> Mir</span><br/>Web Developer</span>
+            <span className='introText'>I'm <span className='introName'> Mir Afzalul Bashar</span><br/>Web Developer</span>
             <p className='introPara'>Weaving code and design to bring websites to life, one pixel at a time.</p>
             <Link activeClass='active' to ='contactPage' spy={true} smooth={true} offset={-70} duration={500} className='contactBtn'>
             <button className='btn'><img src={arrow} alt='arrow' className='arrow'/>Contact me</button>
